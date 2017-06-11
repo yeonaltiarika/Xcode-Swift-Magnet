f48be747ef2690832f5aa3af310de736e86e04b0
Altiarika <yeonaltiarika@users.noreply.github.com>
2017-06-11 21:52:41 +0700

    Update

--- Magnet/ViewController.swift
+++ Magnet/ViewController.swift
@@ -11,19 +11,19 @@ import Magnet
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var magneticView: MagneticView! {
+    @IBOutlet weak var magnetView: MagnetView! {
         didSet {
-            magnetic.magneticDelegate = self
+            magnet.magnetDelegate = self
             #if DEBUG
-                magneticView.showsFPS = true
-                magneticView.showsDrawCount = true
-                magneticView.showsQuadCount = true
+                magnetView.showsFPS = true
+                magnetView.showsDrawCount = true
+                magnetView.showsQuadCount = true
             #endif
         }
     }
     
-    var magnetic: Magnetic {
-        return magneticView.magnetic
+    var magnet: Magnet {
+        return magnetView.magnet
     }
     
     override func viewDidLoad() {
@@ -42,11 +42,11 @@ class ViewController: UIViewController {
     }
     
     @IBAction func reset(_ sender: UIControl?) {
-        let speed = magnetic.physicsWorld.speed
-        magnetic.physicsWorld.speed = 0
-        let sortedNodes = magnetic.children.flatMap { $0 as? Node }.sorted { node, nextNode in
-            let distance = node.position.distance(from: magnetic.magneticField.position)
-            let nextDistance = nextNode.position.distance(from: magnetic.magneticField.position)
+        let speed = magnet.physicsWorld.speed
+        magnet.physicsWorld.speed = 0
+        let sortedNodes = magnet.children.flatMap { $0 as? Node }.sorted { node, nextNode in
+            let distance = node.position.distance(from: magnet.magnetField.position)
+            let nextDistance = nextNode.position.distance(from: magnet.magnetField.position)
             return distance < nextDistance && node.isSelected
         }
         var actions = [SKAction]()
@@ -54,7 +54,7 @@ class ViewController: UIViewController {
             node.physicsBody = nil
             let action = SKAction.run { [unowned magnetic, unowned node] in
                 if node.isSelected {
-                    let point = CGPoint(x: magnetic.size.width / 2, y: magnetic.size.height + 40)
+                    let point = CGPoint(x: magnet.size.width / 2, y: magnet.size.height + 40)
                     let movingXAction = SKAction.moveTo(x: point.x, duration: 0.2)
                     let movingYAction = SKAction.moveTo(y: point.y, duration: 0.4)
                     let resize = SKAction.scale(to: 0.3, duration: 0.4)
@@ -70,21 +70,21 @@ class ViewController: UIViewController {
             let delay = SKAction.wait(forDuration: TimeInterval(index) * 0.01)
             actions.append(delay)
         }
-        magnetic.run(.sequence(actions)) { [unowned magnetic] in
-            magnetic.physicsWorld.speed = speed
+        magnet.run(.sequence(actions)) { [unowned magnetic] in
+            magnet.physicsWorld.speed = speed
         }
     }
     
 }
 
-// MARK: - MagneticDelegate
-extension ViewController: MagneticDelegate {
+// MARK: - MagnetDelegate
+extension ViewController: MagnetDelegate {
     
-    func magnetic(_ magnetic: Magnetic, didSelect node: Node) {
+    func magnet(_ magnet: Magnet, didSelect node: Node) {
         print("didSelect -> \(node)")
     }
     
-    func magnetic(_ magnetic: Magnetic, didDeselect node: Node) {
+    func magnet(_ magnet: Magnet, didDeselect node: Node) {
         print("didDeselect -> \(node)")
     }
     
